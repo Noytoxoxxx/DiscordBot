507b880cadcd9990195659ce67ddf61cb22add36
Noytoxoxxx <137702033+Noytoxoxxx@users.noreply.github.com>
2024-05-07 18:19:10 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- new resource manifest https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/
+fx_version 'bodacious'
+-- https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/#fx-version-bodacious-2020-02
+games {"gta5"}
+
+author "Noytox"
+description "Bot discord qui affiche les joueurs."
+version "1.0.0"
+
+server_script "bot.js"
+
+shared_scripts "config.json"
